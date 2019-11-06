a584c35b3c10282f2da700b475cf9f06cfb61702
Alex Roth <alexroth96@gmail.com>
2019-11-06 10:15:39 -0500

    work on setting state for enemy being hit

--- src/components/gameenvironment/levels/LevelOne.js
+++ src/components/gameenvironment/levels/LevelOne.js
@@ -1,41 +1,49 @@
-import React, { useEffect } from 'react';
-import Enemy from '../players/Enemy';
-import Ground from './Ground';
-import PropTypes from 'prop-types';
+import React, { useEffect } from "react";
+import Enemy from "../players/Enemy";
+import Ground from "./Ground";
+import PropTypes from "prop-types";
 
-const LevelOne = ({ enemies, levelOneEnemies, getDimensions, enemyDimensions, enemyHit, setEnemyAmt, groundRef }) => {
-	useEffect(() => {
-		return setEnemyAmt(levelOneEnemies);
-	}, []);
+const LevelOne = ({
+  enemies,
+  levelOneEnemies,
+  getDimensions,
+  enemyDimensions,
+  enemyHit,
+  setEnemyAmt,
+  groundRef
+}) => {
+  useEffect(() => {
+    return setEnemyAmt(levelOneEnemies);
+  }, []);
 
-	const enemyMap = levelOneEnemies.map((enemy, i) => {
-		return (
-			<Enemy
-				enemyRef={enemy.ref}
-				enemyDimensions={enemyDimensions}
-				getDimensions={getDimensions}
-				enemyHit={enemyHit}
-				key={i}
-				title={enemy.enemy}
-				randomSpawnX={enemy.spawnX}
-				randomSpawnY={enemy.spawnY}
-			/>
-		);
-	});
-	return (
-		<>
-			{enemyMap}
-			<Ground groundRef={groundRef} />
-		</>
-	);
+  const enemyMap = levelOneEnemies.map((enemy, i) => {
+    return (
+      <Enemy
+        enemyRef={enemy.ref}
+        enemyDimensions={enemyDimensions}
+        getDimensions={getDimensions}
+        enemyHit={enemy.hit}
+        key={i}
+        title={enemy.enemy}
+        randomSpawnX={enemy.spawnX}
+        randomSpawnY={enemy.spawnY}
+      />
+    );
+  });
+  return (
+    <>
+      {enemyMap}
+      <Ground groundRef={groundRef} />
+    </>
+  );
 };
 
 LevelOne.propTypes = {
-	groundRef: PropTypes.object.isRequired,
-	levelOneEnemies: PropTypes.array.isRequired,
-	enemyDimensions: PropTypes.array.isRequired,
-	enemyHit: PropTypes.object.isRequired,
-	setEnemyAmt: PropTypes.func.isRequired,
+  groundRef: PropTypes.object.isRequired,
+  levelOneEnemies: PropTypes.array.isRequired,
+  enemyDimensions: PropTypes.array.isRequired,
+  enemyHit: PropTypes.object.isRequired,
+  setEnemyAmt: PropTypes.func.isRequired
 };
 
 export default LevelOne;
