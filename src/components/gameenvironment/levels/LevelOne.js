b0cc5290f9f8ad63b6a5139986b274217db7bc2a
Alex Roth <alexroth96@gmail.com>
2019-11-11 09:36:45 -0500

    setup enemy idle, need to slow movement

--- src/components/gameenvironment/levels/LevelOne.js
+++ src/components/gameenvironment/levels/LevelOne.js
@@ -1,99 +1,97 @@
-import React, { useEffect } from 'react';
-import Enemy from '../players/Enemy';
-import Ground from './Ground';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
+import React, { useEffect } from "react";
+import Enemy from "../players/Enemy";
+import Ground from "./Ground";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
 
 const LevelOne = ({
-	groundHeight,
-	enemyHit,
-	enemyRef1,
-	enemyRef2,
-	getDimensions,
-	enemyDimensions,
-	setEnemyAmt,
-	groundRef,
+  groundHeight,
+  enemyHit,
+  enemyRef1,
+  enemyRef2,
+  getDimensions,
+  enemyDimensions,
+  setEnemyAmt,
+  groundRef
 }) => {
-	const levelOneEnemies = [
-		{
-			enemy: 'enemyOne',
-			ref: enemyRef1,
-			styles: {
-				height: `${1092 / 6}px`,
-				width: `${1712 / 8}px`,
-				top: `${groundHeight.top - 1092 / 6}px`,
-				left: '40vw',
-				position: 'absolute',
-				display: 'block',
-				background: '',
-			},
-			hit: false,
-		},
-		{
-			enemy: 'enemyTwo',
-			ref: enemyRef2,
-			styles: {
-				height: `${1092 / 6}px`,
-				width: `${1712 / 8}px`,
-				top: `${groundHeight.top - 1092 / 6}px`,
-				background: '',
-				position: 'absolute',
-				display: 'block',
-				left: '60vw',
-			},
-			hit: false,
-		},
-	];
-	useEffect(() => {
-		setEnemyAmt(levelOneEnemies);
+  const levelOneEnemies = [
+    {
+      enemy: "enemyOne",
+      ref: enemyRef1,
+      styles: {
+        height: `${1092 / 6}px`,
+        width: `${1712 / 8}px`,
+        top: `${groundHeight.top - 1092 / 6}px`,
+        left: "40vw",
+        position: "absolute",
+        display: "block",
+        background: ""
+      },
+      hit: false
+    },
+    {
+      enemy: "enemyTwo",
+      ref: enemyRef2,
+      styles: {
+        height: `${1092 / 6}px`,
+        width: `${1712 / 8}px`,
+        top: `${groundHeight.top - 1092 / 6}px`,
+        background: "",
+        position: "absolute",
+        display: "block",
+        left: "60vw"
+      },
+      hit: false
+    }
+  ];
+  useEffect(() => {
+    setEnemyAmt(levelOneEnemies);
 
-		return () => setEnemyAmt(levelOneEnemies);
-	}, []);
-	const enemyMap = levelOneEnemies.map((enemy, i) => {
-		return (
-			<Enemy
-				enemyRef={enemy.ref}
-				enemyDimensions={enemyDimensions}
-				getDimensions={getDimensions}
-				key={i}
-				index={i}
-				enemyHit={enemyHit}
-				title={enemy.enemy}
-				enemyHeight={enemy.height}
-				enemyWidth={enemy.width}
-				enemyStyles={enemy.styles}
-			/>
-		);
-	});
-	if (groundHeight.top) {
-		return (
-			<>
-				{enemyMap}
-				<Ground groundRef={groundRef} />
-			</>
-		);
-	}
-	return (
-		<>
-			<Ground groundRef={groundRef} />
-		</>
-	);
+    return () => setEnemyAmt(levelOneEnemies);
+  }, []);
+  const enemyMap = levelOneEnemies.map((enemy, i) => {
+    return (
+      <Enemy
+        enemyRef={enemy.ref}
+        enemyDimensions={enemyDimensions}
+        getDimensions={getDimensions}
+        key={i}
+        index={i}
+        enemyHit={enemyHit}
+        title={enemy.enemy}
+        enemyStyles={enemy.styles}
+      />
+    );
+  });
+  if (groundHeight.top) {
+    return (
+      <>
+        {enemyMap}
+        <Ground groundRef={groundRef} />
+      </>
+    );
+  }
+  return (
+    <>
+      <Ground groundRef={groundRef} />
+    </>
+  );
 };
 
 LevelOne.propTypes = {
-	groundRef: PropTypes.object.isRequired,
-	enemyDimensions: PropTypes.array.isRequired,
-	enemyHit: PropTypes.object.isRequired,
-	setEnemyAmt: PropTypes.func.isRequired,
+  groundRef: PropTypes.object.isRequired,
+  enemyDimensions: PropTypes.array.isRequired,
+  enemyHit: PropTypes.object.isRequired,
+  setEnemyAmt: PropTypes.func.isRequired
 };
 
 const mapStateToProps = state => {
-	return {
-		enemyHit: state.enemy.enemyHit,
-		groundHeight: state.groundHeight.groundHeight,
-		enemy: state.enemy,
-		enemyCoords: state.enemy.enemyCoords,
-	};
+  return {
+    enemyHit: state.enemy.enemyHit,
+    groundHeight: state.groundHeight.groundHeight,
+    enemy: state.enemy,
+    enemyCoords: state.enemy.enemyCoords
+  };
 };
 
 export default connect(mapStateToProps)(LevelOne);
--- src/components/gameenvironment/players/Enemy.js
+++ src/components/gameenvironment/players/Enemy.js
@@ -1,58 +1,76 @@
-import React, { useState, useEffect } from 'react';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import { getEnemyCoordinates } from '../../../actions/enemy';
+import React, { useState, useEffect, useRef } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { getEnemyCoordinates } from "../../../actions/enemy";
 
-const Enemy = ({ enemyRef, getDimensions, enemyHit, title, index, enemyStyles }) => {
-	const [enemyImg, setEnemyImg] = useState('');
-	//possibly get rid of this when more awake
-	const [enemyCoordinates, setEnemyCoordinates] = useState({});
+const Enemy = ({
+  enemyRef,
+  getDimensions,
+  enemyHit,
+  title,
+  index,
+  enemyStyles
+}) => {
+  const [enemyImg, setEnemyImg] = useState("");
+  const [imgPos, setImgPos] = useState(0);
 
-	useEffect(() => {
-		if (enemyRef.current) {
-			setEnemyImg(
-				'https://res.cloudinary.com/snackmanproductions/image/upload/v1573431925/react-game/idle_stufr8.png'
-			);
-			setEnemyCoordinates(enemyRef.current.getBoundingClientRect());
-			getEnemyCoordinates(enemyRef.current.getBoundingClientRect());
-			getDimensions(enemyRef.current.getBoundingClientRect());
-		}
-	}, []);
-	if (!enemyHit[index]) {
-		return (
-			<div
-				className={title}
-				style={{
-					...enemyStyles,
-					background: `url(${enemyImg}) 0px 0px`,
-				}}
-				ref={enemyRef}
-			></div>
-		);
-	}
-	return (
-		<div
-			className={title}
-			style={{
-				...enemyStyles,
-				background: `url(${enemyImg}) 0px 0px`,
-				display: `${enemyHit[index].hit ? 'none' : 'block'}`,
-			}}
-			ref={enemyRef}
-		></div>
-	);
+  const animationRef = useRef();
+
+  const movePosition = time => {
+    const width = 1712 / 8;
+    setImgPos(prevWidth => prevWidth + width);
+    animationRef.current = requestAnimationFrame(movePosition);
+  };
+
+  useEffect(() => {
+    if (enemyRef.current) {
+      setEnemyImg(
+        "https://res.cloudinary.com/snackmanproductions/image/upload/v1573431925/react-game/idle_stufr8.png"
+      );
+      getEnemyCoordinates(enemyRef.current.getBoundingClientRect());
+      getDimensions(enemyRef.current.getBoundingClientRect());
+
+      animationRef.current = requestAnimationFrame(movePosition);
+      return () => cancelAnimationFrame(animationRef.current);
+    }
+  }, []);
+
+  console.log(imgPos);
+  if (!enemyHit[index]) {
+    return (
+      <div
+        className={title}
+        style={{
+          ...enemyStyles,
+          background: `url(${enemyImg}) ${imgPos}px 0px`
+        }}
+        ref={enemyRef}
+      ></div>
+    );
+  }
+  return (
+    <div
+      className={title}
+      style={{
+        ...enemyStyles,
+        background: `url(${enemyImg}) ${imgPos}px 0px`,
+        display: `${enemyHit[index].hit ? "none" : "block"}`
+      }}
+      ref={enemyRef}
+    ></div>
+  );
 };
 
 Enemy.propTypes = {
-	enemyRef: PropTypes.object.isRequired,
-	getDimensions: PropTypes.func.isRequired,
+  enemyRef: PropTypes.object.isRequired,
+  getDimensions: PropTypes.func.isRequired
 };
 
 const mapStateToProps = state => {
-	return { enemyCoords: state.enemy.enemyCoords };
+  return { enemyCoords: state.enemy.enemyCoords, enemy: state.enemy };
 };
 
 export default connect(
-	mapStateToProps,
-	{ getEnemyCoordinates }
+  mapStateToProps,
+  { getEnemyCoordinates }
 )(Enemy);
