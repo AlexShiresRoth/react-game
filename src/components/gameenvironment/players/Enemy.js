c3dadd0e8231b504fa9e93a02ea7b4d7d9ee571a
Alex Rothenberg <alexroth96@gmail.com>
2019-11-14 23:52:54 -0500

    need to figure out how to move enemy in a non insane manner

--- src/components/gameenvironment/players/Enemy.js
+++ src/components/gameenvironment/players/Enemy.js
@@ -1,76 +1,68 @@
-import React, { useState, useEffect, useRef } from "react";
-import PropTypes from "prop-types";
-import { connect } from "react-redux";
-import { getEnemyCoordinates } from "../../../actions/enemy";
+import React, { useState, useEffect, useRef } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { getEnemyCoordinates } from '../../../actions/enemy';
 
-const Enemy = ({
-  enemyRef,
-  getDimensions,
-  enemyHit,
-  title,
-  index,
-  enemyStyles
-}) => {
-  const [enemyImg, setEnemyImg] = useState("");
-  const [imgPos, setImgPos] = useState(0);
+const Enemy = ({ enemyRef, getDimensions, enemyHit, title, index, enemyStyles }) => {
+	const [enemyImg, setEnemyImg] = useState('');
+	const [imgPosX, setImgPosX] = useState(0);
+	const [imgPosY, setImgPosY] = useState(0);
 
-  const animationRef = useRef();
+	const animationRef = useRef();
 
-  const movePosition = time => {
-    const width = 1712 / 8;
-    setImgPos(prevWidth => prevWidth + width);
-    animationRef.current = requestAnimationFrame(movePosition);
-  };
+	const movePosition = time => {
+		const width = 1060 / 5;
+		const height = 1092 / 6;
 
-  useEffect(() => {
-    if (enemyRef.current) {
-      setEnemyImg(
-        "https://res.cloudinary.com/snackmanproductions/image/upload/v1573431925/react-game/idle_stufr8.png"
-      );
-      getEnemyCoordinates(enemyRef.current.getBoundingClientRect());
-      getDimensions(enemyRef.current.getBoundingClientRect());
+		setImgPosX(prevWidth => prevWidth + width);
+		setImgPosY(prevHeight => prevHeight + height);
+		animationRef.current = requestAnimationFrame(movePosition);
+	};
 
-      animationRef.current = requestAnimationFrame(movePosition);
-      return () => cancelAnimationFrame(animationRef.current);
-    }
-  }, []);
+	useEffect(() => {
+		if (enemyRef.current) {
+			setEnemyImg(
+				'https://res.cloudinary.com/snackmanproductions/image/upload/v1573793183/react-game/walk_idwgi0.png'
+			);
+			getEnemyCoordinates(enemyRef.current.getBoundingClientRect());
+			getDimensions(enemyRef.current.getBoundingClientRect());
 
-  console.log(imgPos);
-  if (!enemyHit[index]) {
-    return (
-      <div
-        className={title}
-        style={{
-          ...enemyStyles,
-          background: `url(${enemyImg}) ${imgPos}px 0px`
-        }}
-        ref={enemyRef}
-      ></div>
-    );
-  }
-  return (
-    <div
-      className={title}
-      style={{
-        ...enemyStyles,
-        background: `url(${enemyImg}) ${imgPos}px 0px`,
-        display: `${enemyHit[index].hit ? "none" : "block"}`
-      }}
-      ref={enemyRef}
-    ></div>
-  );
+			animationRef.current = requestAnimationFrame(movePosition);
+			return () => cancelAnimationFrame(animationRef.current);
+		}
+	}, []);
+	if (!enemyHit[index]) {
+		return (
+			<div
+				className={title}
+				style={{
+					...enemyStyles,
+					background: `url(${enemyImg}) ${imgPosX}px ${imgPosY}px`,
+				}}
+				ref={enemyRef}
+			></div>
+		);
+	}
+	return (
+		<div
+			className={title}
+			style={{
+				...enemyStyles,
+				background: `url(${enemyImg}) ${imgPosX}px 0px`,
+				display: `${enemyHit[index].hit ? 'none' : 'block'}`,
+			}}
+			ref={enemyRef}
+		></div>
+	);
 };
 
 Enemy.propTypes = {
-  enemyRef: PropTypes.object.isRequired,
-  getDimensions: PropTypes.func.isRequired
+	enemyRef: PropTypes.object.isRequired,
+	getDimensions: PropTypes.func.isRequired,
 };
 
 const mapStateToProps = state => {
-  return { enemyCoords: state.enemy.enemyCoords, enemy: state.enemy };
+	return { enemyCoords: state.enemy.enemyCoords, enemy: state.enemy };
 };
 
-export default connect(
-  mapStateToProps,
-  { getEnemyCoordinates }
-)(Enemy);
+export default connect(mapStateToProps, { getEnemyCoordinates })(Enemy);
